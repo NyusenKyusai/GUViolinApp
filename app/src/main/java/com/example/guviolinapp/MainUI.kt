011ecf1240acf19e015d3a5e09a6412332ed8fba
Jonah <18018535@uhi.ac.uk>
2020-07-28 07:43:53 +0100

    Second Commit Creation of the MainUI activity. Creation of the Rounded buttons with images. Creating the line barriers between lesson plans

--- app/src/main/java/com/example/guviolinapp/MainUI.kt
+++ app/src/main/java/com/example/guviolinapp/MainUI.kt
@@ -11,6 +11,32 @@ class MainUI : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_u_i)
 
+        btnLevel1.setOnClickListener{
+            val intent = Intent(this, QuizQuestionsActivity::class.java)
+            intent.putExtra("LevelInformation", "Level1")
+            startActivity(intent)
+            finish()
+        }
 
+        btnLevel2.setOnClickListener {
+            val intent = Intent(this, QuizQuestionsActivity::class.java)
+            intent.putExtra("LevelInformation", "Level2")
+            startActivity(intent)
+            finish()
+        }
+
+        btnLevel3.setOnClickListener {
+            val intent = Intent(this, QuizQuestionsActivity::class.java)
+            intent.putExtra("LevelInformation", "Level3")
+            startActivity(intent)
+            finish()
+        }
+
+        btnGame1.setOnClickListener{
+            val intent = Intent(this, QuizGameActivity::class.java)
+            intent.putExtra("GameInformation", "Game1")
+            startActivity(intent)
+            finish()
+        }
     }
 }
